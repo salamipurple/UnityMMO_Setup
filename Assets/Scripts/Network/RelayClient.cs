6f6ab1604fbf525101f5200a9911e48bbe3485a7
Grayson Earle <graysonearle@gmail.com>
2025-08-26 22:49:08 +0200

    updated client script

--- Assets/Scripts/Network/RelayClient.cs
+++ Assets/Scripts/Network/RelayClient.cs
@@ -11,13 +11,12 @@ public class RelayClient : MonoBehaviour
     public string joinCode = "C86DGQ"; // Replace or assign dynamically
     [SerializeField] TextMeshProUGUI joinCodeInput;
 
-    [ContextMenu("Join Relay Game")]
-
     void Update()
     {
-        joinCode = System.Text.RegularExpressions.Regex.Replace(joinCodeInput.text.Trim().ToUpper(), "[^A-Z0-9]", "");
+        //joinCode = System.Text.RegularExpressions.Regex.Replace(joinCodeInput.text.Trim().ToUpper(), "[^A-Z0-9]", "");
 
     }
+    [ContextMenu("Join Relay Game")]
     public async void JoinRelay()
     {
         Debug.Log($"Client: Attempting to join Relay with code: {joinCode}...");
