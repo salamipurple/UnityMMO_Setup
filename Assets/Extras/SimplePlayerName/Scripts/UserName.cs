9ef7a65ab7f0a95a4222ec5f4838af895a325d12
salamipurple <potatoshrek2@gmail.com>
2025-09-16 16:25:09 -0400

    Switched to 'FixedString64Bytes'

--- Assets/Extras/SimplePlayerName/Scripts/UserName.cs
+++ Assets/Extras/SimplePlayerName/Scripts/UserName.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Collections;
 using TMPro;
 
 public class UserName : NetworkBehaviour
@@ -7,32 +8,32 @@ public class UserName : NetworkBehaviour
     [SerializeField] TextMeshPro userNameDisplay;
 
     public static string userName;
-    [SerializeField] private NetworkVariable<string> networkUserName = new NetworkVariable<string>("userName");
+    [SerializeField] private NetworkVariable<FixedString64Bytes> networkUserName = new NetworkVariable<FixedString64Bytes>("userName");
 
     public override void OnNetworkSpawn()
     {
         // Run existing logic in base method
         base.OnNetworkSpawn();
         // Ensure the user name display is up-to-date when a client joins
-        userNameDisplay.text = networkUserName.Value;
+        userNameDisplay.text = networkUserName.Value.ToString();
     }
 
     // Subscribe to score changes when this object is enabled
     void OnEnable()
     {
-        networkUserName.OnValueChanged += OnScoreChanged;
+        networkUserName.OnValueChanged += OnNameChanged;
     }
 
     // Unsubscribe when disabled to avoid memory leaks
     void OnDisable()
     {
-        networkUserName.OnValueChanged -= OnScoreChanged;
+        networkUserName.OnValueChanged -= OnNameChanged;
     }
 
     // Called whenever a user name changes, on all clients
-    private void OnScoreChanged(string previousValue, string newValue)
+    private void OnNameChanged(FixedString64Bytes previousValue, FixedString64Bytes newValue)
     {
         // Update the user name display for everyone
-        userNameDisplay.text = networkUserName.Value;
+        userNameDisplay.text = newValue.ToString();
     }
 }
